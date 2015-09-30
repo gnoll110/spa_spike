5e93a8cb896856d21bdc51720b3e2163c2cdf7b8
Noel Kelly <noel@noelkelly.biz>
2015-09-30 23:50:09 +1000

    add first stub 'describe' and 'it'

--- test/spa.js
+++ test/spa.js
@@ -3,3 +3,13 @@ var should = require('chai').should(),
     benv = require('benv'),
     _ = require('underscore'),
     puzzle = require('../js/puzzle');
+
+describe('Puzzle', function() {
+
+    var puzzle = new Puzzle();
+
+    it('#init', function() {
+        should.exist(puzzle.init);
+    });
+
+});
